e8f7245d34503e973165021d0ee89d6f01692123
arissa@berkeley.edu <arissa@users.noreply.github.com>
2017-11-27 23:29:49 -0800

    added function signatures for Vote.sol and fleshed out some UploadLecture.sol functions

--- contracts/UploadLecture.sol
+++ contracts/UploadLecture.sol
@@ -2,9 +2,44 @@ pragma solidity ^0.4.15;
 
 /** @title UploadLecture */
 contract UploadLecture {
-	
+
+	// mapping from sender's address to list of lecture material urls
+	mapping(address => string[]) userToLectures;
+	address[] users; // list of users who have uploaded lectures
+	string[] lectures; // list of lectures
+	mapping(string => uint) lectureToVotes;
+	uint requiredVotes; // required number of votes for payout
+
+	function UploadLecture(uint threshold) {
+		requiredVotes = threshold;
+	}
+
+	// this uploads a lecture to the app. returns true if successful, 
+	function upload(string lectureUrl) returns (bool) {
+		bool lectureExists = false;
+		for (uint i = 0; i < lectures.length; i++) {
+			// hacky way of comparing strings
+			if (sha3(lectures[i]) == sha3(lectureUrl)) {
+				lectureExists = true;
+			}
+		}
+		if (!lectureExists) {
+			userToLectures[msg.sender].push(lectureUrl);
+			users.push(msg.sender);
+			return true;
+		}
+		return false;
+
+	}
+
+	// tells user whether or not they can receive a payout for a certain lecture right now.
+	// returns true if they can, false if they can't.
+	function payout(string lectureUrl) {
+	}
+
 	/* Fallback function */
 	function() payable {
 		revert();
 	}
+
 }
--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -3,6 +3,18 @@ pragma solidity ^0.4.15;
 /** @title Vote */
 contract Vote {
 	
+
+	// votes for a lecture. a user can't vote for their own lecture.
+	function voteForLecture(string lectureUrl){
+
+	}
+
+	// returns the top n lectures, sorted by votes.
+	function getTopNLectures(uint n) {
+
+	}
+
+
 	/* Fallback function */
 	function() payable {
 		revert();
