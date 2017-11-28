6f51139660d796aeae06832ea1e80771ba2b2d0d
Sarah Young <sarahyoung@Sarahs-MacBook-Pro-4.local>
2017-11-28 00:55:06 -0800

    updated vote

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -2,28 +2,50 @@ pragma solidity ^0.4.15;
 
 /** @title Vote */
 contract Vote {
-	public address[] uploaders;
-	public mapping(String => uint256) votes;
+	address[] public uploaders;
+	mapping(String => uint256) public votes;
+	String[] public lectureList;
 
 	modifier OtherUsersOnly() {
 		bool valid = True;
-		for (int i = 0; i < uploaders.length; i++) {
+		for (uint i = 0; i < uploaders.length; i++) {
 			if (msg.sender.equals(uploaders[i])) {
-				valid = False;
+				valid = false;
 			}
 		}
-	    require(valid == True);
+	    require(valid == true);
 	    _;
 	}
 
+	function Voting(String[] _lectureList) public {
+		//does this work???
+    	lectureList = _lectureList;
+  	}	
 	// votes for a lecture. a user can't vote for their own lecture.
 	function voteForLecture(string lectureUrl) OtherUsersOnly {
+		require(validLecture(lecture));
 		votes[lectureUrl] += 1;
 	}
 
 	// returns the top n lectures, sorted by votes.
 	function getTopNLectures(uint n) {
 		//still need to somehow sort by votes...
+
+	}
+
+	function validLecture(String lecture) public returns (bool) {
+    	for (uint i = 0; i < lectureList.length; i++) {
+      		if (lectureList[i] == lecture) {
+        		return true;
+      		}
+    	}
+    	return false;
+  	}
+
+  	function totalVotes(String lecture) view public returns (uint256) {
+    	require(validLecture(lecture));
+    	return votes[lecture];
+  	}
 
 
 	/* Fallback function */
